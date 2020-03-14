8b8873b20f8b885f7b933b7b3d635eedb4137819
Remy <captainbombycomix>
2020-03-13 18:03:49 -0700

    test

---
+++ Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	public float xspeed;
+	public float gravity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mh = Input.GetAxis ("Horizontal");
+        float mv = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2 (mh*xspeed, mv*gravity);
+        rb.AddForce (movement);
+    }
+}
